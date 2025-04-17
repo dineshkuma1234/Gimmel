7862359f357edf12c4b1e4e1ceedbc2ef9c7f573
harsh <harshvardhan@cosnetglobal.com>
2025-04-17 09:44:50 +0530

    changes

--- src/app/entities/categories/page.js
+++ src/app/entities/categories/page.js
@@ -1,4 +1,5 @@
-'use client'
+"use client"
+
 import React, { useEffect, useState } from 'react';
 import Header from "../../../components/header/header";
 import SliderSection from "../../componentsIn/slidercategories/slider";
